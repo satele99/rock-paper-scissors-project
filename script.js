e8cf228b2a406eaad3846753ea5c92cd9df0627f
Amir Ali <satele.works@gmail.com>
2022-11-18 14:05:55 -0500

    new updates on all files, currently working on js file

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const resButton = document.getElementById('restart-button');
+const scoreButton = document.getElementById('score-button');
+const rockChoice = document.getElementById('rock');
+const paperChoice = document.getElementById('paper');
+const scissorsChoice = document.getElementById('scissors');
+
+const options = ['rock', 'paper', 'scissors'];
+
