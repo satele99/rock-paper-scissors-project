6c79a5a591f686f4f2d9ee6aa6d5db215c219f34
Amir Ali <satele.works@gmail.com>
2022-11-18 20:31:47 -0500

    created a scoreboard, and changed some of the js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 
-const startButton = document.getElementById('start-button');
+const roundButton = document.getElementById('round-button');
 const resButton = document.getElementById('restart-button');
 const scoreButton = document.getElementById('score-button');
 const rockChoice = document.getElementById('rock');
 const paperChoice = document.getElementById('paper');
 const scissorsChoice = document.getElementById('scissors');
-const roundResults = document.getElementById('round-result')
+const roundResults = document.getElementById('round-result');
+const pScore = document.getElementById('p-score');
+const cScore = document.getElementById('c-score');
 const options = ['rock', 'paper', 'scissors'];
 let computerSelection = getComputerChoice();
 let playerSelection = ""; 
@@ -17,18 +19,32 @@ function getComputerChoice() {
     
     return choices;
 };
- 
+function playGame() {
+    if(playerScore == 5 && computerScore < 5){
+        roundResults.innerText = 'Game Over. Player Wins!'
+    }else if(computerScore == 5 && playerScore < 5){
+        roundResults.innerText = 'Game Over. Computer Wins!'
+    }
+};
+
+pScore.innerText = playerScore;
+cScore.innerText = computerScore;
+
 rockChoice.addEventListener('click', event => {
     playerSelection = 'rock';
     computerSelection = getComputerChoice();
     console.log(computerSelection);
     if (computerSelection == 'scissors'){
-        roundResults.innerText = 'Player Wins!'
+        roundResults.innerText = 'Player Wins! Rock beats Scissors.'
         playerScore++
+        pScore.innerText = playerScore;
+        cScore.innerText = computerScore;
         return
     }else if(computerSelection == 'paper'){
-        roundResults.innerText = 'Computer Wins!';
+        roundResults.innerText = 'Computer Wins! Paper beats Rock.';
         computerScore++
+        pScore.innerText = playerScore;
+        cScore.innerText = computerScore;
         return
     }else if(computerSelection == 'rock'){
         roundResults.innerText = 'Draw!';
@@ -40,12 +56,16 @@ paperChoice.addEventListener('click', event => {
     computerSelection = getComputerChoice();
     console.log(computerSelection);
     if (computerSelection == 'scissors'){
-        roundResults.innerText = 'Computer Wins!';
+        roundResults.innerText = 'Computer Wins! Scissors beats Paper.';
         computerScore++
+        pScore.innerText = playerScore;
+        cScore.innerText = computerScore;
         return
     }else if(computerSelection == 'rock'){
-        roundResults.innerText = 'Player Wins!'
+        roundResults.innerText = 'Player Wins! Paper beats Rock.'
         playerScore++
+        pScore.innerText = playerScore;
+        cScore.innerText = computerScore;
         return
     }else if (computerSelection == 'paper'){
         roundResults.innerText = 'Draw!';
@@ -58,12 +78,16 @@ scissorsChoice.addEventListener('click', event => {
     computerSelection = getComputerChoice();
     console.log(computerSelection);
     if(computerSelection == 'rock') {
-        roundResults.innerText = 'Computer Wins!';
+        roundResults.innerText = 'Computer Wins! Rock beats Scissors.';
         computerScore++
+        pScore.innerText = playerScore;
+        cScore.innerText = computerScore;
         return
     }else if(computerSelection == 'paper'){
-        roundResults.innerText = 'Player Wins!'
+        roundResults.innerText = 'Player Wins! Scissors beats Paper.'
         playerScore++
+        pScore.innerText = playerScore;
+        cScore.innerText = computerScore;
         return
     }else if(computerSelection == 'scissors'){
         roundResults.innerText = 'Draw!';
@@ -73,9 +97,7 @@ scissorsChoice.addEventListener('click', event => {
 });
 
 
-    startButton.addEventListener('click', playGame => {
-        
-    }); 
+
 
     scoreButton.addEventListener('click',event => {
         alert('Player Score:'+ " "+ playerScore + " "+ '||' + " "+ 'Computer Score:' + " "+ computerScore);
@@ -84,9 +106,12 @@ scissorsChoice.addEventListener('click', event => {
     resButton.addEventListener('click', event => {
         playerScore = 0;
         computerScore = 0;
-        alert('Player Score:'+ " "+ playerScore + " "+ '||' + " "+ 'Computer Score:' + " "+ computerScore);
-        roundResults.innerText = "";
+        pScore.innerText = playerScore;
+        cScore.innerText = computerScore;
+        // alert('Player Score:'+ " "+ playerScore + " "+ '||' + " "+ 'Computer Score:' + " "+ computerScore);
+        roundResults.innerText = ""; 
     }) 
+
 
     
 
